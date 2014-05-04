7ea2a5d2e2185bec544b265e9362fd99cc29cb99
Liu Lantao <liulantao@gmail.com>
2014-05-04 17:12:01 +0800

    init

---
+++ app.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+require 'open-uri'
+require 'json'
+
+class AppController < Sinatra::Base
+  RSS_URL = {
+    "beijing" => "http://www.stateair.net/web/rss/1/1.xml",
+    "chengdu" => "http://www.stateair.net/web/rss/1/2.xml",
+    "guangzhou" => "http://www.stateair.net/web/rss/1/3.xml",
+    "shanghai" => "http://www.stateair.net/web/rss/1/4.xml",
+    "shenyang" => "http://www.stateair.net/web/rss/1/5.xml"
+  }
+
+  get '/' do
+    ''
+  end
+
+  get '/aqi/:city.json' do
+    @info = {:status => -1, :data => [], :msg => ""}
+    @city = params[:city]
+
+    if RSS_URL.keys.include?(@city)
+      begin
+        open(RSS_URL[@city]) do |xml|
+          feed = Nokogiri::XML(xml)
+          feed.xpath("//item").each do |item|
+            title = item.xpath("title").text
+            conc = item.xpath("Conc").text
+            aqi = item.xpath("AQI").text
+            desc = item.xpath("Desc").text
+            timestamp = item.xpath("ReadingDateTime").text
+            timestamp = Time.parse(timestamp).to_i
+
+            @info[:data] << {
+              :city=> @city,
+              :timestamp => timestamp,
+              :title => title,
+              :conc => conc,
+              :aqi => aqi,
+              :desc => desc
+            }
+          end
+        end
+        @info[:status] = 0
+        @info[:msg] = "Success!"
+      rescue
+        @info[:status] = 1
+        @info[:msg] = "Server Error!"
+      end
+
+    else
+      @info[:status] = 2
+      @info[:msg] = "City Not Found!"
+
+    end
+
+    content_type 'application/json'
+    @info.to_json
+  end
+end
